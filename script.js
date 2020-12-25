9f1672658d88253baaf26ca78af97e57310dc1b5
Thom711 <tvdveldt@hotmail.com>
2020-12-25 14:50:46 +0100

    first commit, final commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// DEEL A
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+  
+ikRockArrowFunctions();
+
+
+const ikRockArrowFunctions2 = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions2();
+
+
+const ikRockArrowFunctions3 = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions3();
+
+
+// DEEL B
+const fivePlusSeven = function () {
+    return 5 + 7
+};
+  
+fivePlusSeven();
+
+
+const fivePlusSeven2 = () => 5 + 7;
+
+fivePlusSeven2();
+
+
+// DEEL C
+const myFunction = (a, b) => a + b;
+myFunction(4, 3);
+
+
+// DEEL D
+const addFive = a => a + 5;
+addFive(12);
+
+
+// DEEL E
+const createObject = () => ({greeting: "hoi"});
+createObject();
